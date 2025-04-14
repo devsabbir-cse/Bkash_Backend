273ba55c8f5f2ba92fe83d0f3e7a34b91cf63945
devsabbir-cse <your-email@example.com>
2025-04-14 11:45:13 +0600

    Your commit message here

--- api/index.js
+++ api/index.js
@@ -1,49 +1,24 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const authRoutes = require("../routes/authRoutes");
-
-// dotenv.config();
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// // Routes
-// app.use("/api", authRoutes);
-
-// // // Start the server
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// // module.exports = app; // Export the app for Vercel
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRoutes = require("../routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
 
 // Middleware
-app.use(cors(
-
-));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Welcome to the API!");
-});
-
-// Import routes
-const authRoutes = require("../routes/authRoutes");
 app.use("/api", authRoutes);
 
-module.exports = app;
+// // Start the server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app; // Export the app for Vercel
--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,11 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host:'fdb28.awardspace.net',
-  user: '4592095_bkash', // e.g., 'root'
-  password: '?LCsm2m3L8EfK65',
-  database: '4592095_bkash'
-  // host:'localhost',
-  // user: 'root', // e.g., 'root'
-  // password: '',
-  // database: 'Bkash'
+ 
+  host:'localhost',
+  user: 'root', // e.g., 'root'
+  password: '',
+  database: 'bkash'
 });
 
 db.connect((err) => {
